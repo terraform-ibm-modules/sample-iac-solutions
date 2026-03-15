9f5da9c718425e5673187ac4a7e2d17ef60aba49
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-03-15 04:34:50 +0000

    fix(deps): update dependencies

--- containerized_app_landing_zone/version.tf
+++ containerized_app_landing_zone/version.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     restapi = {
       source  = "Mastercard/restapi"
-      version = "2.0.1"
+      version = "3.0.0"
     }
     time = {
       source  = "hashicorp/time"
@@ -11,7 +11,7 @@ terraform {
     }
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.87.3"
+      version = "1.89.0"
     }
     helm = {
       source  = "hashicorp/helm"
