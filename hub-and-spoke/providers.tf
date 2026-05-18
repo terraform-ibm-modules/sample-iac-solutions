ac2ae0b7f145de0256d46afc08267f09c9417dd8
mounicatellabati <78633728+mounicatellabati@users.noreply.github.com>
2026-05-18 17:05:03 +0530

    Update providers.tf

--- hub-and-spoke/providers.tf
+++ hub-and-spoke/providers.tf
@@ -1,7 +1,4 @@
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.region
-  # Increase default timeouts for VPC resources
-  max_retries      = 10
-  ibmcloud_timeout = 3600
 }
