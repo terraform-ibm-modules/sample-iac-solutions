471ea5fb762dcee8468045a7180f3b5a93231163
Khuzaima Shakeel <56439894+Khuzaima05@users.noreply.github.com>
2026-09-15 18:05:04 +0530

    fix(deps): update dependencies (#77)

--- secure-infra-ai-app/main.tf
+++ secure-infra-ai-app/main.tf
@@ -147,7 +147,7 @@ module "key_protect_all_inclusive" {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "10.17.9"
+  version                = "10.17.11"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cos_instance_name      = "${var.prefix}-my-cos"
