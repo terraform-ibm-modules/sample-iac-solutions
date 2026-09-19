43cf1583adecbe42f566ebe3b242271f4be75d0f
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-09-19 15:25:06 +0000

    fix(deps): update dependencies

--- hub-and-spoke/main.tf
+++ hub-and-spoke/main.tf
@@ -18,7 +18,7 @@ module "latest_ubuntu_image" {
 # Each subnet gets a `/22` block (e.g., `10.0.0.0/22`, `10.0.4.0/22`, `10.0.8.0/22`)
 module "management_vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "10.0.6"
+  version           = "10.0.8"
   resource_group_id = module.resource_group.resource_group_id
   region            = "us-south"
   name              = "${var.prefix}-management-vpc"
@@ -100,7 +100,7 @@ module "management_vpc" {
 # All other traffic is implicitly denied by default, creating a secure, isolated environment for your application servers.
 module "workload_vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "10.0.6"
+  version           = "10.0.8"
   resource_group_id = module.resource_group.resource_group_id
   region            = "us-south"
   name              = "${var.prefix}-workload-vpc"
@@ -192,7 +192,7 @@ resource "ibm_is_ssh_key" "ssh_key" {
 # The assigned IP is available in the jumpbox_public_ip output.
 module "jumpbox_server" {
   source                = "terraform-ibm-modules/landing-zone-vsi/ibm"
-  version               = "6.6.2"
+  version               = "7.0.1"
   create_security_group = true
   image_id              = module.latest_ubuntu_image.latest_image_id # Dynamically fetched latest Ubuntu image
   enable_floating_ip    = true
@@ -244,7 +244,7 @@ module "jumpbox_server" {
 
 module "workload_servers" {
   source                = "terraform-ibm-modules/landing-zone-vsi/ibm"
-  version               = "6.6.2"
+  version               = "7.0.1"
   create_security_group = true
   image_id              = module.latest_ubuntu_image.latest_image_id # Dynamically fetched latest Ubuntu image
   security_group = {
@@ -431,7 +431,7 @@ module "workload_vpe_security_group" {
 
 module "workload_vpes" {
   source             = "terraform-ibm-modules/vpe-gateway/ibm"
-  version            = "5.4.0"
+  version            = "5.4.2"
   region             = "us-south"
   prefix             = "${var.prefix}-workload-vpe"
   vpc_name           = module.workload_vpc.vpc_name
@@ -450,7 +450,7 @@ module "workload_vpes" {
 
 module "cos_storage" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "10.17.11"
+  version                = "10.17.17"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = "us-south"
   cos_instance_name      = "${var.prefix}-cos-storage"
--- terragrunt/vpc/terragrunt.hcl
+++ terragrunt/vpc/terragrunt.hcl
@@ -8,7 +8,7 @@ include "variables" {
 }
 
 terraform {
-  source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone-vpc.git?ref=v10.0.6"
+  source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone-vpc.git?ref=v10.0.8"
 }
 
 dependency "resource_group" {
